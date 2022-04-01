dc44b785a8aa139b1c305192577cb5d4bf241458
Joshua Hveem <jhveem@btech.edu>
2022-04-01 12:12:44 -0600

    update lti info

--- lti-data.js
+++ lti-data.js
@@ -1,8 +1,8 @@
-const export = {
-  client_secret: "",
-  client_id: "",
-  lti_name: "",
-  lti_url_base: ""
+const exports = {
+  client_secret: "0cNK6P5IacCSXcOKnr0zSc4A3L2TzPIqx2UoeJxZh4Swesku3NdotC3F0CyFb1r5",
+  client_id: "140000000000318",
+  lti_name: "course track",
+  lti_url_base: "https://course-track.bridgetools.dev"
 }
 
-module.exports = export;
+module.exports = exports;
